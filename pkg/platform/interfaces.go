fb23aaf4e722ba8cb06cf1f0b862949be9a69b7a
Jialei <jialeicui@126.com>
2024-10-20 10:55:50 +0800

    chore: remove useless platform impl (#6)

--- pkg/platform/interfaces.go
+++ pkg/platform/interfaces.go
@@ -1,6 +1,7 @@
 package platform
 
 type Platform interface {
+	Name() string
 	// Sync synchronizes the platform with the latest data.
 	// It should be called periodically to keep the platform up-to-date.
 	Sync() error
